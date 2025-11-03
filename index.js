585932cc9a4c91f889cf5fe5222b9e2516ee26dc
Sanjeev <ms.saisanjeev@gmail.com>
2025-11-03 10:39:27 +0530

    normal

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const PORT=process.env.PORT || 3000;
 
 app.use(cors({
       origin: process.env.FRONTEND_URL, 
-      credentials: true               
+      credentials: true,             
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
